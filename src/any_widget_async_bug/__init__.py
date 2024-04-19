0ce0597b137711e2c267afca2240e6742436fef4
Xen0Xys <red57101@gmail.com>
2024-04-19 13:35:52 +0000

    :test_tube: Add not working base to fix the bug

--- src/any_widget_async_bug/__init__.py
+++ src/any_widget_async_bug/__init__.py
@@ -1,9 +1,11 @@
 import importlib.metadata
 import pathlib
+import time
+from typing import Any
 
 import anywidget
 import traitlets
-from traitlets import Int
+from traitlets import Int, observe
 
 try:
     __version__ = importlib.metadata.version("any_widget_async_bug")
@@ -17,5 +19,17 @@ class BugWidget(anywidget.AnyWidget):
 
     value = Int(0).tag(sync=True)
 
+    def __init__(self, *args: Any, **kwargs: Any):
+        super().__init__(*args, **kwargs)
+        self.value_computing = False
+
     def compute_value(self):
+        self.value_computing = True
         self.send({"event_name": "compute_value"})
+        while self.value_computing:
+            time.sleep(0.1)
+        return self.value
+
+    @observe("value")
+    def _observe_value(self, change):
+        self.value_computing = False
