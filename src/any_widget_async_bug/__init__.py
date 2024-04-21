b43a2edf090f307a4f4ba2750e9a09378fdc22da
ManonMarchand <manonmarchand22@gmail.com>
2024-04-21 10:55:58 +0200

    trigget end of cell within compute_value
    failure so far

--- src/any_widget_async_bug/__init__.py
+++ src/any_widget_async_bug/__init__.py
@@ -2,8 +2,9 @@ import importlib.metadata
 import pathlib
 
 import anywidget
-import traitlets
 from traitlets import Int
+from IPython import get_ipython
+from inspect import isawaitable
 
 try:
     __version__ = importlib.metadata.version("any_widget_async_bug")
@@ -17,5 +18,15 @@ class BugWidget(anywidget.AnyWidget):
 
     value = Int(0).tag(sync=True)
 
-    def compute_value(self):
+    async def compute_value(self):
         self.send({"event_name": "compute_value"})
+        kernel = get_ipython().kernel
+        try:
+            end_of_cell = kernel.do_one_iteration()
+            if isawaitable(end_of_cell):
+                await end_of_cell
+        except Exception:
+            return
+        finally:
+            kernel.set_parent(kernel._parent_ident,
+                              kernel.get_parent())
